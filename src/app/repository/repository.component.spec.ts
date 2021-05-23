299bcdaa0a2a8b097bbb66be4834dc34ff32ba5f
TechVictorKE <victor.maina@student.moringaschool.com>
2021-05-23 21:41:54 +0300

    Updated angular

--- src/app/repository/repository.component.spec.ts
+++ src/app/repository/repository.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RepositoryComponent } from './repository.component';
 
@@ -6,7 +6,7 @@ describe('RepositoryComponent', () => {
   let component: RepositoryComponent;
   let fixture: ComponentFixture<RepositoryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RepositoryComponent ]
     })
